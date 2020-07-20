dc94ff8d916a25d8e57b67fd6c58b6045f6daf88
Abhilash Mishra <mishraabhi.sdr@gmail.com>
2020-07-20 08:30:04 +0530

    tests Updated

--- BookStore-1/src/main/java/com/cg/bookStore/exceptions/CategoryException.java
+++ BookStore-1/src/main/java/com/cg/bookStore/exceptions/CategoryException.java
@@ -2,6 +2,11 @@ package com.cg.bookStore.exceptions;
 
 public class CategoryException extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7697553224715869338L;
+
 	public CategoryException() {
 		super();
 	}
--- BookStore-1/src/main/java/com/cg/bookStore/web/BookStoreExceptionAdvice.java
+++ BookStore-1/src/main/java/com/cg/bookStore/web/BookStoreExceptionAdvice.java
@@ -7,18 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.cg.bookStore.exceptions.BookException;
 import com.cg.bookStore.exceptions.CategoryException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class BookStoreExceptionAdvice {
-	
+		
 		@ExceptionHandler(value= {CategoryException.class, BookException.class})
-		@ResponseStatus(code=HttpStatus.NOT_FOUND)
+		@ResponseStatus(code=HttpStatus.BAD_REQUEST)
 		public Map<String,String> handleException(Exception ex) {
 			Map<String, String> map = new HashMap<>();
 			map.put("message", ex.getMessage());
 			return map;
 		}
-	}
+}
